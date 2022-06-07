dcf231407ece90bebfa712a381d2b9a390820af6
Myztillx <samuel.geist@gmail.com>
2022-06-07 09:40:07 -0400

    Create single file out of rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,16 @@
-import typescript from 'rollup-plugin-typescript2';
-import commonjs from 'rollup-plugin-commonjs';
-import nodeResolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
-import { terser } from 'rollup-plugin-terser';
-import serve from 'rollup-plugin-serve';
-import json from '@rollup/plugin-json';
-import ignore from './rollup-plugins/ignore';
-import { ignoreTextfieldFiles } from './elements/ignore/textfield';
-import { ignoreSelectFiles } from './elements/ignore/select';
-import { ignoreSwitchFiles } from './elements/ignore/switch';
+import typescript from 'rollup-plugin-typescript2'
+import commonjs from 'rollup-plugin-commonjs'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+import { terser } from 'rollup-plugin-terser'
+import serve from 'rollup-plugin-serve'
+import json from '@rollup/plugin-json'
+import ignore from './rollup-plugins/ignore'
+import { ignoreTextfieldFiles } from './elements/ignore/textfield'
+import { ignoreSelectFiles } from './elements/ignore/select'
+import { ignoreSwitchFiles } from './elements/ignore/switch'
 
-const dev = process.env.ROLLUP_WATCH;
+const dev = process.env.ROLLUP_WATCH
 
 const serveopts = {
   contentBase: ['./dist'],
@@ -20,7 +20,7 @@ const serveopts = {
   headers: {
     'Access-Control-Allow-Origin': '*',
   },
-};
+}
 
 const plugins = [
   nodeResolve({}),
@@ -33,9 +33,13 @@ const plugins = [
   dev && serve(serveopts),
   !dev && terser(),
   ignore({
-    files: [...ignoreTextfieldFiles, ...ignoreSelectFiles, ...ignoreSwitchFiles].map((file) => require.resolve(file)),
+    files: [
+      ...ignoreTextfieldFiles,
+      ...ignoreSelectFiles,
+      ...ignoreSwitchFiles,
+    ].map((file) => require.resolve(file)),
   }),
-];
+]
 
 export default [
   {
@@ -45,5 +49,6 @@ export default [
       format: 'es',
     },
     plugins: [...plugins],
+    inlineDynamicImports: true,
   },
-];
+]
